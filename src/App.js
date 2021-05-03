65e6af235d8b999a4e73bab755480de770672049
ajtejas <tejasjain559@gmail.com>
2021-05-04 00:17:12 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.compoent";
 import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
